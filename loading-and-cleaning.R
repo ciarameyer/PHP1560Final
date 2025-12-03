126ead2d45642c8815bbc4df2e1cd18686737062
ciarameyer <ciara_meyer@brown.edu>
2025-12-03 13:17:56 -0500

    Update loading-and-cleaning.R

--- loading-and-cleaning.R
+++ loading-and-cleaning.R
@@ -20,7 +20,7 @@ unique(otp_data$Route)
 unique(ridership_data$Route)
 
 # mutating OTP data to only include needed variables 
-otp_data <- subset(otp_data, select = - c(Driver.ID, Stop, Stop.Sequence, 
+otp_data <- subset(otp_data, select = - c(Driver.ID, Trip, Stop, Stop.Sequence, 
                                           StopLat, StopLng)) 
 
 # mutating ridership data to only include needed variables 
@@ -32,6 +32,15 @@ ridership_data <- ridership_data %>%
   mutate(Child = if_else(Type == "Child", 1, 0)) %>%
   mutate(Senior = if_else(Type == "Senior", 1, 0)) %>%
   mutate(Disabled = if_else(Type == "Disabled", 1, 0)) %>%
-  mutate(Brown-RISD = if_else(College == "Brown" | "RISD", 1, 0)))
-  
+  mutate(Brown_RISD = if_else(College == "Brown" | College ==  "RISD", 1, 0))  %>%
+  mutate(Other_College = if_else(College == "None" | College != "Brown" | 
+                                   College != "RISD", 1, 0)) %>%
+  mutate(High_School = if_else(High.School != "None", 1, 0))
+
+ridership_data <- subset(ridership_data, select = c(Time, Day.of.Week, Route, 
+                                                    Low.Income, Child, Senior, 
+                                                    Disabled, Brown_RISD, 
+                                                    Other_College, High_School))
+
+
 
