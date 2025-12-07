8a8b5bb9ab1461139e2a67cdf18afad55960ceb8
ciarameyer <ciara_meyer@brown.edu>
2025-12-07 11:06:13 -0500

    updates

--- correlation-and-hypothesis-tests.R
+++ correlation-and-hypothesis-tests.R
@@ -5,14 +5,18 @@ correlations <- function(df, threshold = 0.6, use = "everything"){
   res <- data.frame(name1 = vector("character"),
                     name2 = vector("character"),
                     cor = vector("numeric"))
+  
+  # get rid of any NaN
+  df <- select_if(df, is.numeric)
+  
   # find correlations 
-  cor_mat <- cor.test(df, use = use)
+  cor_mat <- cor(df, use = use)
   
   # find pairs with high correlations 
   n <- nrow(cor_mat)
-  for( i in 1(n-1)){
+  for( i in 1:(n-1)){
     for(j in (i + 1):n){
-      if(abs(cor_mat[i,j]) > threshold){
+      if(!is.na(cor_mat[i, j]) && abs(cor_mat[i, j]) > threshold){
         res <- add_row(res,
                        name1 = colnames(cor_mat)[i],
                        name2 = colnames(cor_mat)[j],
@@ -23,10 +27,12 @@ correlations <- function(df, threshold = 0.6, use = "everything"){
   return(res)
 }
 
-correlation_data_peak <- correlations(df = merged_data_peak, threshold = 0.6, 
+correlation_data_peak <- correlations(df = merged_data_peak, threshold = 0.4, 
                                       use = "everything")
 correlation_data_off_peak <- correlations(df = merged_data_off_peak,
-                                          threshold = 0.6, use = "everything")
+                                          threshold = 0.4, use = "everything")
+correlation_data_overall <- correlations(df = merged_data,
+                                          threshold = 0.4, use = "everything")
 
 t_tests <- function(df, threshold = 0.05, use = "everything"){
   # results data frame
@@ -34,22 +40,20 @@ t_tests <- function(df, threshold = 0.05, use = "everything"){
                     name2 = vector("character"),
                     t_test = vector("numeric"))
   
-  # find t tests
-  t_mat <- t.test(df, use = use)
   
-  # find pairs with statistically significant t tests
-  n <- nrow(t_mat)
-  for( i in 1(n-1)){
+  # find t tests and pairs with statistically significant t tests
+  for( i in 1:(n-1)){
     for(j in (i + 1):n){
-      if(abs(t_mat[i,j]) < threshold){
+      test_result <- t.test(df[[i]], df[[j]], use = use)
+     if(test_result$p.value < threshold){
         res <- add_row(res,
-                       name1 = colnames(t_mat)[i],
-                       name2 = colnames(t_mat)[j],
-                       t_test = t_mat[i,j])
+                       name1 = colnames(df)[i],
+                       name2 = colnames(df)[j],
+                       t_test_p_value = test_result$p.value)
       }
     }
   }
   return(res)
 }
 
-t_tests_data <- t_tests(merged_data)
+t_tests_data <- t_tests(merged_data_peak)
--- merging-outputs-for-final-analysis.R
+++ merging-outputs-for-final-analysis.R
@@ -1,7 +1,10 @@
 # Step 5: Merging data frames (for peak and non-peak)
 
-merged_data_peak <- full_join(route_performance_results, peak_summary,
+merged_data_peak <- right_join(route_performance_results, peak_summary,
                          by = "Route")
 
-merged_data_off_peak <- full_join(route_performance_results, off_summary,
+merged_data_off_peak <- right_join(route_performance_results, off_summary,
                                   by = "Route")
+
+merged_data <- right_join(route_performance_results, overall_summary,
+                          by = "Route")
